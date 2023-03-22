fca994517238e39620bf19de4d0f4bafad8f2428
Fábio de Paula <60741276+fabiopjg@users.noreply.github.com>
2023-03-21 23:40:37 -0300

    Delete grupo

--- grupo/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Grupo(models.Model):
-    nomeGrupo = models.CharField(max_length=100)
-    biografiaGrupo = models.CharField(max_length=500)
-    fotoCapaGrupo = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
