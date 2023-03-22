c28035e53f41e9dd14e7611cc6611f2a7a31d4f0
Fábio de Paula <60741276+fabiopjg@users.noreply.github.com>
2023-03-21 23:39:44 -0300

    Delete directory

--- chat/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Chat(models.Model):
-    data = models.DateTimeField()
-    horario = models.IntegerField(default=0)
-    visualizacao = models.IntegerField(default=0)
