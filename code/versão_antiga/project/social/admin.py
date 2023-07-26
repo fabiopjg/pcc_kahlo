fa296237e04b0aaa5542072ca8886d17f69eda37
Fábio de Paula <60741276+fabiopjg@users.noreply.github.com>
2023-07-26 10:20:10 -0300

    Deletado versao_antiga

--- code/versão_antiga/project/social/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post, UserPerfil, Comentario, ConexaoModel
-
-admin.site.register(Post)
-admin.site.register(UserPerfil)
-admin.site.register(Comentario)
-admin.site.register(ConexaoModel)
-# Register your models here.
